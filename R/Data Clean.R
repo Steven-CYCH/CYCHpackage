785f0ee97cfb5fb7166db11ef58e9665f9e976f1
Steven(acer) <cych15334@gmail.com>
2024-07-14 15:29:29 +0800

    1.0.1 function s.dc.outlier_detector modifty string output NA

--- R/Data Clean.R
+++ R/Data Clean.R
@@ -9,20 +9,20 @@ s.dc.outlier_detector <- function(DT, ID_name = 'ID', sig_num = 3, NA_obs_out = 
     # NA_omit 列出離群值的過程中是否一併列出遺漏值(missing)
     # in_list 需要被檢測的變數名稱，以c('', '')輸入
     # out_list 不被檢測的變數名稱，以c('', '')輸入
-    
+
     # DT <- DT.id
     # ID_name <- 'ID'
     # sig_num <- 3
     # NA_obs_out <- FALSE
     # in_list <- colnames(dataset)
     # out_list <- NULL
-    
+
     dataset <- copy(DT)
-    
+
     if (ID_name %in% colnames(dataset)){
         dataset <- as.data.table(dataset)
         out_list <- append(out_list, ID_name)
-        
+
         # 整理需要被檢測的變數
         if (is.null(in_list)){
             in_list <- colnames(dataset)[-which(colnames(dataset) %in% out_list)]
@@ -33,7 +33,7 @@ s.dc.outlier_detector <- function(DT, ID_name = 'ID', sig_num = 3, NA_obs_out = 
                 in_list <- in_list
             }
         }
-        
+
         # 設定離群值的上下界
         for (variable in in_list) {
             # variable <- 'sodium'
@@ -60,7 +60,7 @@ s.dc.outlier_detector <- function(DT, ID_name = 'ID', sig_num = 3, NA_obs_out = 
             options(warn = 1)
             upper_bound <- mean_value + standard_deviation * sig_num
             lower_bound <- mean_value - standard_deviation * sig_num
-            
+
             if ((is.na(upper_bound) & is.na(lower_bound)) != TRUE){
                 # 針對各觀察值檢測其是否超出離群值範圍
                 for (i in 1:dim(dataset)[1]) {
@@ -76,9 +76,16 @@ s.dc.outlier_detector <- function(DT, ID_name = 'ID', sig_num = 3, NA_obs_out = 
                     }
                 }
             }else{
-                cat('該變數觀察值型態非為數值\n')
+              cat('該變數觀察值型態非為數值\n')
+              for (i in 1:dim(dataset)[1]) {
+                obs <- dataset[i, variable]
+                if (is.na(obs)){
+                  if (NA_obs_out == TRUE){
+                    cat('ID是', dataset[[ID_name]][i], '的紀錄中有變數', variable, '的觀察值為 Missing Data \n')
+                  }
+                }
+              }
             }
-        }
     }else{
         stop('親愛的朋友，你的ID不是這個名字喔！')
     }
@@ -92,19 +99,19 @@ s.dc.missing_detector <- function(DT, listout_col = NULL, NA_obs_out = FALSE){
     # ID_name ID不被納入檢測，以'字串'型態輸入
     # listout_col 檢測過程中需列出參考的欄位，以c('', '')輸入
     # NA_obs_out 檢測過程中是否列出NA的觀察值
-    
+
     # DT <- DT.exam
     # ID_name <- 'ID'
     # listout_col <- NULL
     # NA_obs_out <- TRUE
-    
+
     dataset <- copy(as.data.table(DT))
     # 以下未修改
-    
+
     if (is.null(listout_col)){
         listout_col <- colnames(dataset)
     }
-    
+
     DT <- as.data.frame(dataset)
     missing_count <- 0
     for (colName in colnames(DT)){
@@ -122,8 +129,8 @@ s.dc.missing_detector <- function(DT, listout_col = NULL, NA_obs_out = FALSE){
                 missDT <- DT[is.na(obs) == TRUE, listout_col]
             }
         }
-        
-        
+
+
         if (dim(missDT)[1] != 0){
             missing_count <- missing_count + 1
             missing.ratio <- (dim(missDT)[1]/dim(DT)[1]) * 100
@@ -151,9 +158,9 @@ s.dc.sample_missing <- function(DT, deleting_ratio_over = 0.1){
     # dataset 要檢查遺漏值的dataset名稱，以data.table形式輸入
     # deleting_ratio 樣本觀察值超過該比例的遺失刪除，以小數輸入
     # The reference doi is：10.1007/s00432-022-04063-5
-    
+
     dataset <- copy(as.data.table(DT))
-    
+
     dataset <- cbind(cacheID = 1:dim(dataset)[1], dataset)
     analysis_transpose <- data.table(t(dataset))
     names(analysis_transpose) <- as.character(dataset$cacheID)
@@ -162,7 +169,7 @@ s.dc.sample_missing <- function(DT, deleting_ratio_over = 0.1){
     include_pt <- as.numeric(as.vector(analysis_m[analysis_m['pct_missing'] <= deleting_ratio_over, colnames(analysis_m)[1]]))
     dataset <- dataset[dataset$cacheID %in% include_pt, ]
     dataset <- subset(dataset, select = -cacheID)
-    
+
     return(dataset)
 }
 
@@ -175,9 +182,9 @@ s.dc.missing_imputation <- function(DT, impute_list = NULL, exclude_list = NULL,
     # exclude_list 不需要做填補的變數名稱，若有ID需包含ID
     # impute_method 做填補的方法，可選mean(平均)、median(中位數)或mode(眾數)
     # decimal 小數位數
-    
+
     dataset <- copy(as.data.table(DT))
-    
+
     # 整理需要被填補的變數
     if (is.null(impute_list) & is.null(exclude_list)){
         stop('請輸入要填補的變數名稱，或至少指定一個不填補的變數(若有ID需指定ID)')
@@ -188,14 +195,14 @@ s.dc.missing_imputation <- function(DT, impute_list = NULL, exclude_list = NULL,
     }else{
         impute_list <- impute_list[-which(impute_list %in% exclude_list)]
     }
-    
+
     dataset <- as.data.frame(dataset)
     for (variable in impute_list) {
         cat('\n')
         cat(variable)
         observation <- dataset[[variable]]
         na_num <- length(observation[is.na(observation)])
-        
+
         if (na_num != 0){
             if (impute_method == 'mean'){
                 impute_value <- round(mean(observation, na.rm = TRUE), digits = decimal)
