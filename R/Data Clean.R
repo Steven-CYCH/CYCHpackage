4a5e833ee0b3afcb61e176c8bc50094ef1790af2
Steven <cych15334@gmail.com>
2024-07-16 15:45:13 +0800

    1.0.2 function s.dc.missing_detector modifty string type to improve interpretability

--- R/Data Clean.R
+++ R/Data Clean.R
@@ -80,8 +80,10 @@ s.dc.outlier_detector <- function(DT, ID_name = 'ID', sig_num = 3, NA_obs_out = 
 
 
 # 遺漏值偵測 ----
-s.dc.missing_detector <- function(DT, listout_col = NULL, NA_obs_out = FALSE){
-    dataset <- copy(as.data.table(DT[, lapply(.SD, as.character)]))
+s.dc.missing_detectorr <- function(DT, listout_col = NULL, NA_obs_out = FALSE){
+    DT <- copy(as.data.frame(DT))
+    DT <- data.frame(lapply(DT, as.character), stringsAsFactors = FALSE)
+    dataset <- copy(as.data.table(DT))
 
     if (is.null(listout_col)){
         listout_col <- colnames(dataset)
