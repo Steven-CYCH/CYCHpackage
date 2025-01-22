a9abed06002deabacea70bd5b4cee0aefb84949b
Steven <cych15334@gmail.com>
2025-01-22 09:21:18 +0800

    3.1.1 function addition s.mt.adjust.glm

--- R/Model tips.R
+++ R/Model tips.R
@@ -1,17 +1,9 @@
-library(data.table)
-
-set.seed(1234)
-test.data <- data.table(var1 = sample(0:100, 50, replace = TRUE),
-                        var2 = sample(c('Yes', 'No'), 50, replace = TRUE),
-                        var3 = sample(0:100, 50, replace = TRUE))
+# library(data.table)
+#
+# DT <- fread('C:\\Users\\kkk\\Desktop\\logistic_regression_data.csv')
+# DT[, `:=`(X8 = factor(X8), X9 = factor(X9), X10 = factor(X10))]
 
 s.mt.reduce.lm <- function(datasets, var.y, var.x = c(), threshold = 0.1, del.var.x = c()){
-
-  # datasets <- test.data
-  # var.y <- 'var3'
-  #
-  # threshold = 0.3
-  # del.var.x = c()
 
   datasets <- as.data.table(datasets)
 
@@ -56,4 +48,59 @@ s.mt.reduce.lm <- function(datasets, var.y, var.x = c(), threshold = 0.1, del.va
 }
 
 
-lm <- s.mt.reduce.lm(datasets = test.data, var.y = 'var2')
+# lm <- s.mt.reduce.lm(datasets = test.data, var.y = 'var2')
+
+s.mt.adjust.glm <- function(datasets, var.y, var.x = c(), confounder = c(), threshold = 0.1, del.var.x = c()){
+
+  # datasets = DT
+  # var.y = 'Y1'
+  #
+  # var.x = c()
+  #
+  # threshold = 0.4
+  # del.var.x = c('X1', 'X2')
+  # confounder = paste0('Z', 1:5)
+
+  datasets <- as.data.table(datasets)
+
+  if (is.null(var.x)){
+    var.x <- colnames(datasets)[!(colnames(datasets) %in% c(var.y, del.var.x, confounder))]
+  }else{
+    var.x <- var.x[!(var.x %in% c(var.y, del.var.x, confounder))]
+  }
+
+  simple.p <- data.table(var = NULL, p = NULL)
+
+  for (simplevar in var.x){
+    # simplevar <- var.x[6]
+    varlist <- c(var.y, confounder, simplevar)
+    DT.simple <- datasets[, ..varlist]
+
+    simple.glm.fr <- formula(paste0(var.y, ' ~ ', paste(c(confounder, simplevar), collapse = ' + ')))
+    simple.glm <- glm(simple.glm.fr, data = DT.simple, family = 'binomial')
+
+    glm.summary <- as.data.frame(summary(simple.glm)$coefficients)
+    var.name <- rownames(glm.summary)[grepl(simplevar, rownames(glm.summary))]
+    var.p <- glm.summary['Pr(>|z|)'][var.name, ]
+
+    simple.p.cache <- data.table(var = simplevar, p = min(var.p))
+
+    simple.p <- rbind(simple.p, simple.p.cache)
+  }
+
+  var.x.adj.glm <- simple.p[p < threshold, var][!duplicated(simple.p[p < threshold, var])]
+
+  if (length(var.x.adj.glm) == 0){
+    stop('There is no significance variable in Crude logistic regression model')
+  }else{
+    fr.adj.glm <- paste(c(confounder, var.x.adj.glm), collapse = ' + ')
+    fr.adj.glm <- formula(paste0(var.y, ' ~ ', fr.adj.glm))
+
+    adj.glm <- glm(fr.adj.glm, data = datasets, family = 'binomial')
+    # summary(adj.glm)
+
+    return(adj.glm)
+  }
+}
+
+# glm <- s.mt.adjust.glm(datasets = DT, var.y = 'Y1', confounder = paste0('Z', 1:5), threshold = 0.7)
